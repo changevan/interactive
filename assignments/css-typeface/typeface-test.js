dcc8f5b59525460293474cea96bafe17e482a2d0
Evan Chang <evan.chang@yale.edu>
2018-04-02 14:58:27 -0400

    fixed javascript

--- assignments/css-typeface/typeface-test.js
+++ assignments/css-typeface/typeface-test.js
@@ -1,12 +1,40 @@
-var width = Math.floor(window.innerWidth);
-var height = Math.floor(window.innerHeight);
-var x = width + "deg";
-var y = width/5 + "deg";
-
 $(document).ready(function(){
 
+  function rotate() {
+    var width = Math.floor(window.innerWidth);
+    var height = Math.floor(window.innerHeight);
+    var x = width/-20 + "deg";
+    var y = width/50 + "deg";
+    var w = width/50 + "deg";
+    $('.left-one').css('transform', 'rotate(' + x + ')');
+    $('.left-two').css('transform', 'rotate(' + y + ')');
+    $('.right-one').css('transform', 'rotate(' + w + ')');
+
+  }
+
     $(window).resize(function(){
-        $('.left-one').css('transform', 'rotate(' + x + ')');
-        $('.left-two').css('transform', 'rotate(' + y + ')');
+      rotate();
     });
+
 });
+//
+//
+// $(document).ready(function() {
+//
+//   function fontSize() {
+//     width = Math.floor(window.innerWidth);
+//     height = Math.floor(window.innerHeight);
+//     $("#width").html(width);
+//     $("#height").html(height);
+//     $("#width").css("font-size", width/3.3 + 'px');
+//     $("#height").css("font-size", height/4.3 + 'px');
+//   }
+//
+//
+//   $(window).resize(function() {
+//   	fontSize();
+//   });
+//
+//   fontSize();
+//
+// });
